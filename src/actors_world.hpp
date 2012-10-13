ea6d2c0ee7feb49042ad7d2e8e474b58ce340d5e
Jonathan Fort <jonathan.fort@etu.univ-orleans.fr>
2012-10-14 01:16:26 +0200

    ajouté actors_world.hpp

---
+++ src/actors_world.hpp
@@ -0,0 +1,23 @@
+#ifndef DEF_ACTORS_WORLD
+#define DEF_ACTORS_WORLD
+
+class actors_world: public basic_world{
+
+  actors_world(int i=0, j=0, int nb): basic_world(i, j), nbActors(nb){
+    list = new actor[nbActors];
+  }
+
+  actors_world(const actors_world& m): basic_world(m){
+    this->nbActors=m.nbActors;
+    this->list= new actor[nbActors];
+  }
+
+  actor_world& operator=(const actor_world& m){
+  }
+
+
+
+protected:
+  actor * list;
+  int nbActors;
+};
