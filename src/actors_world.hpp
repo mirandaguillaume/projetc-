7cae9fa211546e0696d9c1baeef4a491c504a7be
Jonathan Fort <jonathan.fort@etu.univ-orleans.fr>
2012-10-14 02:46:19 +0200

    ajouté les methodes move à actors_world.hpp

--- src/actors_world.hpp
+++ src/actors_world.hpp
@@ -1,23 +1,66 @@
 #ifndef DEF_ACTORS_WORLD
 #define DEF_ACTORS_WORLD
 
+
 class actors_world: public basic_world{
 
   actors_world(int i=0, j=0, int nb): basic_world(i, j), nbActors(nb){
-    list = new actor[nbActors];
+    actors = new actor[nbActors];
   }
 
   actors_world(const actors_world& m): basic_world(m){
     this->nbActors=m.nbActors;
-    this->list= new actor[nbActors];
+    this->actors= new actor[nbActors];
   }
 
-  actor_world& operator=(const actor_world& m){
+  actors_world& operator=(const actors_world& m){
+    basic_world::operator=(m);
+    if (this!=&m){
+      this->nbActors=m.nbActors;
+      this->actors= new actor[nbActors];
+    }
   }
 
+  ~actors_world(){
+    if (actors)
+      delete [] actors;
+  }
+
+  bool occuped(int x, int y){
+    return (matrix[x][y]>0);
+  }
+
+  //L'axe XY suit l'axe cartésien |XY inversé dans la matrice
+void move_up(actor* a)
+{
+  matrix[a->getY()][a->getX]*=-1;
+  a->setY((a->getY()+1)%height);
+  matrix[a->getY()][a->getX]*=-1;
+}
+
+void move_down(actor* a)
+{
+  matrix[a->getY()][a->getX]*=-1;
+  a->setY((a->getY()-1+height)%height);
+  matrix[a->getY()][a->getX]*=-1
+}
+
+void move_right(actor* a)
+{
+  matrix[a->getY()][a->getX]*=-1;
+  a->setX((a->getX()+1)%length);
+  matrix[a->getY()][a->getX]*=-1;
+}
+
+void move_left(actor*a)
+{
+  matrix[a->getY()][a->getX]*=-1;
+  a->setX((a->getX()-1+length)%length);
+  matrix[a->getY()][a->getX]*=-1;
+}
 
 
 protected:
-  actor * list;
+  actor * actors;
   int nbActors;
 };
