0d11d15fefbed05a2f1167da05bde69f590cf3b2
Jonathan Fort <jonathan.fort@etu.univ-orleans.fr>
2012-10-14 23:22:26 +0200

    ajouté ant.hpp et modifié ants_world.hpp

--- src/actors_world.hpp
+++ src/actors_world.hpp
@@ -27,36 +27,6 @@ class actors_world: public basic_world{
       delete [] actors;
   }
 
-  bool occuped(int x, int y){
-    return (matrix[x][y]<0);
-  }
-
-  //L'axe XY suit l'axe cartésien |XY inversé dans la matrice
-void move_up(actor* a){
-  matrix[a->getY()][a->getX]*=-1;
-  a->setY((a->getY()+1)%height);
-  matrix[a->getY()][a->getX]*=-1;
-}
-
-void move_down(actor* a){
-  matrix[a->getY()][a->getX]*=-1;
-  a->setY((a->getY()-1+height)%height);
-  matrix[a->getY()][a->getX]*=-1
-}
-
-void move_right(actor* a){
-  matrix[a->getY()][a->getX]*=-1;
-  a->setX((a->getX()+1)%length);
-  matrix[a->getY()][a->getX]*=-1;
-}
-
-void move_left(actor*a){
-  matrix[a->getY()][a->getX]*=-1;
-  a->setX((a->getX()-1+length)%length);
-  matrix[a->getY()][a->getX]*=-1;
-}
-
-
 protected:
   actor * actors;
   int nbActors;
