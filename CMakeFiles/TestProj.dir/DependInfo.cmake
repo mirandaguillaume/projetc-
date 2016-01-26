3ce0f4d3464694ee625768fa9a9356dfbf121315
Guillaume Miranda <guillaume11miranda@gmail.com>
2016-01-26 11:28:03 +0100

    Migration to git

---
+++ CMakeFiles/TestProj.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/guillaume/bzr_projects/projetc++/GUI/Fenetre_jeu.cpp" "/home/guillaume/bzr_projects/projetc++/CMakeFiles/TestProj.dir/GUI/Fenetre_jeu.cpp.o"
+  "/home/guillaume/bzr_projects/projetc++/GUI/QSFMLCanvas.cpp" "/home/guillaume/bzr_projects/projetc++/CMakeFiles/TestProj.dir/GUI/QSFMLCanvas.cpp.o"
+  "/home/guillaume/bzr_projects/projetc++/GUI/fenetre.cpp" "/home/guillaume/bzr_projects/projetc++/CMakeFiles/TestProj.dir/GUI/fenetre.cpp.o"
+  "/home/guillaume/bzr_projects/projetc++/GUI/jeu.cpp" "/home/guillaume/bzr_projects/projetc++/CMakeFiles/TestProj.dir/GUI/jeu.cpp.o"
+  "/home/guillaume/bzr_projects/projetc++/GUI/menu.cpp" "/home/guillaume/bzr_projects/projetc++/CMakeFiles/TestProj.dir/GUI/menu.cpp.o"
+  "/home/guillaume/bzr_projects/projetc++/GUI/moc_Fenetre_jeu.cxx" "/home/guillaume/bzr_projects/projetc++/CMakeFiles/TestProj.dir/GUI/moc_Fenetre_jeu.cxx.o"
+  "/home/guillaume/bzr_projects/projetc++/GUI/moc_QSFMLCanvas.cxx" "/home/guillaume/bzr_projects/projetc++/CMakeFiles/TestProj.dir/GUI/moc_QSFMLCanvas.cxx.o"
+  "/home/guillaume/bzr_projects/projetc++/GUI/moc_fenetre.cxx" "/home/guillaume/bzr_projects/projetc++/CMakeFiles/TestProj.dir/GUI/moc_fenetre.cxx.o"
+  "/home/guillaume/bzr_projects/projetc++/GUI/moc_jeu.cxx" "/home/guillaume/bzr_projects/projetc++/CMakeFiles/TestProj.dir/GUI/moc_jeu.cxx.o"
+  "/home/guillaume/bzr_projects/projetc++/GUI/moc_menu.cxx" "/home/guillaume/bzr_projects/projetc++/CMakeFiles/TestProj.dir/GUI/moc_menu.cxx.o"
+  "/home/guillaume/bzr_projects/projetc++/src/ants_world.cpp" "/home/guillaume/bzr_projects/projetc++/CMakeFiles/TestProj.dir/src/ants_world.cpp.o"
+  "/home/guillaume/bzr_projects/projetc++/src/cells_world.cpp" "/home/guillaume/bzr_projects/projetc++/CMakeFiles/TestProj.dir/src/cells_world.cpp.o"
+  "/home/guillaume/bzr_projects/projetc++/src/main.cpp" "/home/guillaume/bzr_projects/projetc++/CMakeFiles/TestProj.dir/src/main.cpp.o"
+  "/home/guillaume/bzr_projects/projetc++/src/termites_world.cpp" "/home/guillaume/bzr_projects/projetc++/CMakeFiles/TestProj.dir/src/termites_world.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "QT3_SUPPORT"
+  "QT_3SUPPORT_LIB"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_XML_LIB"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/qt4"
+  "/usr/include/qt4/Qt3Support"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtXml"
+  "/usr/include/qt4/QtCore"
+  "."
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ CMakeFiles/TestProj.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "GUI/moc_menu.cxx"
+  "GUI/moc_QSFMLCanvas.cxx"
+  "GUI/moc_fenetre.cxx"
+  "GUI/moc_jeu.cxx"
+  "GUI/moc_Fenetre_jeu.cxx"
+  "CMakeFiles/TestProj.dir/GUI/menu.cpp.o"
+  "CMakeFiles/TestProj.dir/GUI/fenetre.cpp.o"
+  "CMakeFiles/TestProj.dir/GUI/QSFMLCanvas.cpp.o"
+  "CMakeFiles/TestProj.dir/GUI/jeu.cpp.o"
+  "CMakeFiles/TestProj.dir/src/main.cpp.o"
+  "CMakeFiles/TestProj.dir/src/ants_world.cpp.o"
+  "CMakeFiles/TestProj.dir/src/cells_world.cpp.o"
+  "CMakeFiles/TestProj.dir/src/termites_world.cpp.o"
+  "CMakeFiles/TestProj.dir/GUI/Fenetre_jeu.cpp.o"
+  "CMakeFiles/TestProj.dir/GUI/moc_menu.cxx.o"
+  "CMakeFiles/TestProj.dir/GUI/moc_QSFMLCanvas.cxx.o"
+  "CMakeFiles/TestProj.dir/GUI/moc_fenetre.cxx.o"
+  "CMakeFiles/TestProj.dir/GUI/moc_jeu.cxx.o"
+  "CMakeFiles/TestProj.dir/GUI/moc_Fenetre_jeu.cxx.o"
+  "/home/guillaume/bzr_projects/bin/TestProj.pdb"
+  "/home/guillaume/bzr_projects/bin/TestProj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestProj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
